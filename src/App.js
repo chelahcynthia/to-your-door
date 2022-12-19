3a477adba828feca221e30a63ffb21a5c33db94f
cynthia <cchelangat9892gmail.com>
2022-12-19 13:59:49 +0300

    Styled the navbar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Cart from "./components/Cart";
+import Profile from "./components/Profile";
 import LogIn from "./components/LogIn";
 import NavBar from "./components/NavBar";
 
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/cart" element={<Cart />} />
+        <Route path="/profile" element={<Profile />} />
         <Route path="/login" element={<LogIn />} />
       </Routes>
     </>

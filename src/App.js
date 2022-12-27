0cdae361d8152a363115a4e561cd77ddd509f120
cynthia <cchelangat9892gmail.com>
2022-12-27 18:36:27 +0300

    Created buttons and styled them

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./components/Home";
 import Cart from "./components/Cart";
 import Profile from "./components/Profile";
 import LogIn from "./components/LogIn";
+import SignUp from "./components/SignUp"
 import NavBar from "./components/NavBar";
 import "./App.css";
 
@@ -16,6 +17,7 @@ function App() {
         <Route path="/cart" element={<Cart />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/login" element={<LogIn />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
     </>
   );
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import './Home.css';
+
 
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <div className="home-container">
+      <button className="delivery-btn">
+        <i className="fa fa-map-marker"></i>
+          Enter Delivery Address
+      </button>
+      <button className="order-btn">
+        <i className="fa fa-cutlery"></i>Order Now
+      </button>
+    </div>
+  );
 }
 
-export default Home
+export default Home;

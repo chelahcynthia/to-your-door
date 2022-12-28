57b4f8ec1476dbd45a531ad47839b211726f1f6b
cynthia <cchelangat9892gmail.com>
2022-12-28 16:30:01 +0300

    Created a sign up form

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -2,8 +2,23 @@ import React from 'react'
 
 function SignUp() {
   return (
-    <div>
-        <h1>Hello</h1>
+    <div className='sign-container'>
+        <div className='sign-header'>
+          <h3>Hi there! Order today</h3>
+          <h1>To Your Door!</h1>
+        </div>
+        <div className='main-content'>
+          <form>
+          <input type="text" name="" placeholder='Full name'></input>
+          <input type="email" name="" placeholder='Email'></input>
+          <input type="text" name="" placeholder='Username'></input>  
+          <input type="password" name="" placeholder='Password'></input>
+          <input type="password" name="" placeholder='Confirm Password'></input> 
+          <button type='submit' className='sign-btn'>Signup</button> 
+
+
+          </form>
+        </div>
         </div>
   )
 }

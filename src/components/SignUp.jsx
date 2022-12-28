022ef5412c4acdcacc61eae9ff0dd42c12557ff6
cynthia <cchelangat9892gmail.com>
2022-12-28 17:46:22 +0300

    Done with the signup form

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,27 +1,31 @@
-import React from 'react'
-import './SignUp.css';
+import React from "react";
+import "./SignUp.css";
 
 function SignUp() {
   return (
-    <div className='sign-container'>
-        <div className='sign-header'>
-          <h3>Hi there! Order today</h3>
-          <h1>To Your Door!</h1>
-        </div>
-        <div className='main-content'>
-          <form>
-          <input type="text" name="" placeholder='Full name'></input>
-          <input type="email" name="" placeholder='Email'></input>
-          <input type="text" name="" placeholder='Username'></input>  
-          <input type="password" name="" placeholder='Password'></input>
-          <input type="password" name="" placeholder='Confirm Password'></input> 
-          <button type='submit' className='sign-btn'>Signup</button> 
-
-
-          </form>
-        </div>
-        </div>
-  )
+    <div className="sign-container">
+      <div className="sign-header">
+        <h3>Hi there! Order today</h3>
+        <h1>To Your Door!</h1>
+      </div>
+      <div className="main-content">
+        <form>
+          <input type="text" name="" placeholder="Full name"></input>
+          <input type="email" name="" placeholder="Email"></input>
+          <input type="text" name="" placeholder="Username"></input>
+          <input type="password" name="" placeholder="Password"></input>
+          <input type="password" name="" placeholder="Confirm Password"></input>
+          <div className="sign-link">
+            <button type="submit" className="sign-btn">
+              Signup
+            </button>
+            <p>Already have an account?</p>
+            <a href="#">Login</a>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default SignUp
+export default SignUp;

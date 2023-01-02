557cfc496d811ab0049d7760cbdbc68477a736d0
JeremyKungu <jeremikungu27@gmail.com>
2023-01-02 12:48:44 +0300

    added headers

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -2,7 +2,30 @@ import React from 'react'
 
 function Profile() {
   return (
-    <div>Profile</div>
+    <div className='profile'>
+      <div className='prof-header'>
+        <h3>Profile</h3>
+        <i class="fa fa-user-circle" aria-hidden="true"></i>
+      </div>
+
+      <div className='prof-container'>
+        <h1>Hi, Chelah!</h1>
+        <p>We have a new meal in Restaurant 02! Try it now!</p>
+      </div>
+
+      <h3>Your Favorite Meals</h3>
+      <div className='meal-cards'>
+
+      </div>
+      <div className='prof-user'>
+
+      </div>
+
+      <div className='meal-description'>
+        
+      </div>
+
+    </div>
   )
 }
 

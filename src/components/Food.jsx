f801f23d5e2c9c18eca71e37ec39ce653023388f
Pronepoet <ochiengphillip84@gmail.com>
2023-01-10 09:34:15 +0300

    Adding changes

--- src/components/Food.jsx
+++ src/components/Food.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Button from 'react-bootstrap/Button';
 import { Routes, Route } from "react-router-dom";
 import Card from 'react-bootstrap/Card';
 import CardGroup from 'react-bootstrap/CardGroup';
@@ -7,43 +8,40 @@ function Food(){
         <div>
 <CardGroup>
       <Card>
-        <Card.Img variant="top" src="holder.js/100px160" />
+        <Card.Img variant="top" src="./images/face.png" />
         <Card.Body>
-          <Card.Title>Card title</Card.Title>
+          <Card.Title>Fried Chicken</Card.Title>
           <Card.Text>
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content. This content is a little bit longer.
+           This chicken is locally sourced from our patner smallholder farmers.
           </Card.Text>
         </Card.Body>
         <Card.Footer>
-          <small className="text-muted">Last updated 3 mins ago</small>
+         
+          <Button variant="primary">Add to Cart</Button>
+        </Card.Footer>
+      </Card>
+      <Card>
+        <Card.Img variant="top" src="https://images.pexels.com/photos/1146760/pexels-photo-1146760.jpeg?auto=compress&cs=tinysrgb&w=1600" />
+        <Card.Body>
+          <Card.Title>French Fries</Card.Title>
+          <Card.Text>
+         
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer>
+        <Button variant="primary">Add to Cart</Button>
         </Card.Footer>
       </Card>
       <Card>
         <Card.Img variant="top" src="holder.js/100px160" />
         <Card.Body>
-          <Card.Title>Card title</Card.Title>
+          <Card.Title>Plain Rice</Card.Title>
           <Card.Text>
-            This card has supporting text below as a natural lead-in to
-            additional content.{' '}
+          
           </Card.Text>
         </Card.Body>
         <Card.Footer>
-          <small className="text-muted">Last updated 3 mins ago</small>
-        </Card.Footer>
-      </Card>
-      <Card>
-        <Card.Img variant="top" src="holder.js/100px160" />
-        <Card.Body>
-          <Card.Title>Card title</Card.Title>
-          <Card.Text>
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content. This card has even longer content than the
-            first to show that equal height action.
-          </Card.Text>
-        </Card.Body>
-        <Card.Footer>
-          <small className="text-muted">Last updated 3 mins ago</small>
+        <Button variant="primary">Add to Cart</Button>
         </Card.Footer>
       </Card>
     </CardGroup>

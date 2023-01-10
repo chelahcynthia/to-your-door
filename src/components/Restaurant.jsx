309525d29ee39cc264529f7bc741e41961231f7f
Joy Chemutai <joy.chemutai@student.moringaschool.com>
2023-01-10 14:55:33 +0300

    add restaurant styling

--- src/components/Restaurant.jsx
+++ src/components/Restaurant.jsx
@@ -1,14 +1,25 @@
 import React from 'react'
+import { GiKnifeFork } from "react-icons/gi";
 import Res1 from '../images/Res1.jpg'
 import Res2 from '../images/Res2.jpg'
+import Res3 from '../images/Res3.jpg'
+import Res4 from '../images/Res4.jpg'
+import Res5 from '../images/Res5.jpg'
+import Res6 from '../images/Res6.jpg'
 import "./Restaurant.css"
 
 function Restaurant(){
   return (
     <>
-      <h2 className='restaurant-heading'> Restaurants </h2>
-        <img src={Res1} alt="image1"></img>
-        
+      <h2 className='restaurant-heading'> Restaurants <GiKnifeFork/> </h2>
+        <div className='picture-div'>
+          <img src={Res1} alt="image1" className='image=1'></img>
+          <img src={Res2} alt="image2"></img>
+          <img src={Res3} alt="image3"></img>
+          <img src={Res4} alt="image4"></img>
+          <img src={Res5} alt="image5"></img>
+          <img src={Res6} alt="image6"></img>
+        </div>
     </>
   )
 }

e70e48efde580ebeddb2dd5c7287d26742e8bbb4
Joy Chemutai <joy.chemutai@student.moringaschool.com>
2023-01-11 22:46:53 +0300

    add redirect to food page

--- src/components/Restaurant.jsx
+++ src/components/Restaurant.jsx
@@ -6,6 +6,7 @@ import Res3 from '../images/Res3.jpg'
 import Res4 from '../images/Res4.jpg'
 import Res5 from '../images/Res5.jpg'
 import Res6 from '../images/Res6.jpg'
+import { Link } from "react-router-dom";
 import "./Restaurant.css"
 
 function Restaurant(){
@@ -13,12 +14,14 @@ function Restaurant(){
     <>
       <h2 className='restaurant-heading'> Restaurants <GiKnifeFork/> </h2>
         <div className='picture-div'>
-          <img src={Res1} alt="image1" className='image-1'></img>
-          <img src={Res2} alt="image2" className= 'image-2'></img>
-          <img src={Res3} alt="image3" className= 'image-3'></img>
-          <img src={Res4} alt="image4" className= 'image-4'></img>
-          <img src={Res5} alt="image5" className= 'image-5'></img>
-          <img src={Res6} alt="image6" className= 'image-6'></img>
+          <Link to="/food" className="food-btn">
+            <img src={Res1} alt="image1" className='image-1'></img>
+            <img src={Res2} alt="image2" className= 'image-2'></img>
+            <img src={Res3} alt="image3" className= 'image-3'></img>
+            <img src={Res4} alt="image4" className= 'image-4'></img>
+            <img src={Res5} alt="image5" className= 'image-5'></img>
+            <img src={Res6} alt="image6" className= 'image-6'></img>
+          </Link>
         </div>
     </>
   )

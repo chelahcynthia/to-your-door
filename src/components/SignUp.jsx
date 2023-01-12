e4314ddc70beee42cca269202aa17c62d52238dc
cynthia <cchelangat9892gmail.com>
2023-01-12 11:42:11 +0300

    Created a link to login

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import "./SignUp.css";
 
 function SignUp() {
+  const [formData, setFormData] = useState({
+    first_name: "",
+    email: "",
+    username: "",
+    password: "",
+    password_confirmation: "",
+  });
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
   return (
     <div className="sign-container">
       <div className="sign-header">
@@ -10,17 +23,59 @@ function SignUp() {
       </div>
       <div className="main-content">
         <form>
-          <input type="text" name="" placeholder="Full name"></input>
-          <input type="email" name="" placeholder="Email"></input>
-          <input type="text" name="" placeholder="Username"></input>
-          <input type="password" name="" placeholder="Password"></input>
-          <input type="password" name="" placeholder="Confirm Password"></input>
+          <input
+            type="text"
+            name=""
+            onChange={(e) =>
+              setFormData({ ...formData, first_name: e.target.value })
+            }
+            placeholder="Full name"
+          ></input>
+          <input
+            type="email"
+            name=""
+            onChange={(e) =>
+              setFormData({ ...formData, email: e.target.value })
+            }
+            placeholder="Email"
+          ></input>
+          <input
+            type="text"
+            name=""
+            onChange={(e) =>
+              setFormData({ ...formData, username: e.target.value })
+            }
+            placeholder="Username"
+          ></input>
+          <input
+            type="password"
+            name=""
+            onChange={(e) =>
+              setFormData({ ...formData, password: e.target.value })
+            }
+            placeholder="Password"
+          ></input>
+          <input
+            type="password"
+            name=""
+            onChange={(e) =>
+              setFormData({
+                ...formData,
+                password_confirmation: e.target.value,
+              })
+            }
+            placeholder="Confirm Password"
+          ></input>
           <div className="sign-link">
-            <button type="submit" className="sign-btn">
+            <button
+              type="submit"
+              onClick={(e) => handleSubmit(e)}
+              className="sign-btn"
+            >
               Signup
             </button>
             <p>Already have an account?</p>
-            <a href="#">Login</a>
+            <Link to="/login">Login</Link>
           </div>
         </form>
       </div>

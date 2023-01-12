0cb134b3625b8f9dbdd9da1cc00169715a04dec3
Joy Chemutai <joy.chemutai@student.moringaschool.com>
2023-01-13 02:29:30 +0300

    add hover effect

--- src/components/Restaurant.jsx
+++ src/components/Restaurant.jsx
@@ -15,12 +15,46 @@ function Restaurant(){
       <h2 className='restaurant-heading'> Restaurants <GiKnifeFork/> </h2>
         <div className='picture-div'>
           <Link to="/food" className="food-btn">
-            <img src={Res1} alt="image1" className='image-1'></img>
-            <img src={Res2} alt="image2" className= 'image-2'></img>
-            <img src={Res3} alt="image3" className= 'image-3'></img>
-            <img src={Res4} alt="image4" className= 'image-4'></img>
-            <img src={Res5} alt="image5" className= 'image-5'></img>
-            <img src={Res6} alt="image6" className= 'image-6'></img>
+            <div className='container'>
+              <img src={Res1} alt="image1" className='image-1'></img>
+              <div class="one">
+                <div class="text">LeBistro</div>
+              </div>
+            </div>
+            <div className='container'>
+              <img src={Res2} alt="image2" className= 'image-2'></img>
+              <div class="two">
+                <div class="text">
+                    Beer
+                      vs
+                    Wine
+                </div>
+              </div>
+            </div>
+            <div className='container'>
+              <img src={Res3} alt="image3" className= 'image-3'></img>
+              <div class="three">
+                <div class="text">Donut Dip</div>
+              </div>
+            </div>
+            <div className='container'>
+              <img src={Res4} alt="image4" className= 'image-4'></img>
+              <div class="four">
+                <div class="text">Bomonti</div>
+              </div>
+            </div>
+            <div className='container'>
+              <img src={Res5} alt="image5" className= 'image-5'></img>
+              <div class="five">
+                <div class="text">Annies Harbour</div>
+              </div>
+            </div>
+            <div className='container'>
+              <img src={Res6} alt="image6" className= 'image-6'></img>
+              <div class="six">
+                <div class="text">Jeff's</div>
+              </div>
+            </div>
           </Link>
         </div>
     </>

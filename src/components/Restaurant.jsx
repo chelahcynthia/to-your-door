80cf644ef04b67768467fc70a091b901ff3c2bfd
JeremyKungu <jeremikungu27@gmail.com>
2023-01-13 10:45:35 +0300

    styling

--- src/components/Restaurant.jsx
+++ src/components/Restaurant.jsx
@@ -7,8 +7,8 @@ function Restaurant(){
   return (
     <>
       <h2 className='restaurant-heading'> Restaurants <GiKnifeFork/> </h2>
-        <div className='picture-div'>
-          {/* <Link to="/food" className="food-btn"> */}
+      <Link to="/food" className="food-btn">
+        <div className='picture-div'>          
             <div className='img-card1'>
               <img src="https://images.pexels.com/photos/14212506/pexels-photo-14212506.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" className='img-1'/>
             </div>
@@ -18,11 +18,12 @@ function Restaurant(){
             </div>
 
             <div className='img-card3'>
-              <img src="https://images.pexels.com/photos/11838704/pexels-photo-11838704.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" className='img-3'/>
+              <img src="https://images.pexels.com/photos/3352765/pexels-photo-3352765.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" className='img-3'/>
             </div>
 
             <div className='img-card4'>
-              <img src="https://images.pexels.com/photos/3352765/pexels-photo-3352765.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" className='img-4'/>
+              <img src="https://images.pexels.com/photos/11838704/pexels-photo-11838704.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" className='img-4'/>
+              
             </div>
 
             <div className='img-card5'>
@@ -31,18 +32,10 @@ function Restaurant(){
 
             <div className='img-card6'>
               <img src="https://images.pexels.com/photos/2574489/pexels-photo-2574489.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" className='img-6'/>
-            </div>
-                        
-          {/* </Link> */}
-        </div>
+            </div>                    
+         </div>
+        </Link>
     </>
   )
 }
 export default Restaurant
-
-{/* <img src="" alt="image1" className='image-1'></img>
-            <img src="" alt="image2" className= 'image-2'></img>
-            <img src="" alt="image3" className= 'image-3'></img>
-            <img src="" alt="image4" className= 'image-4'></img>
-            <img src="" alt="image5" className= 'image-5'></img>
-            <img src="" alt="image6" className= 'image-6'></img> */}

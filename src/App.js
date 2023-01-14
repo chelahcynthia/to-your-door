389b8fba74608e9416d9794260f55c20d076458a
+ <+>
2023-01-14 14:50:40 +0300

    fixed all the merge conflicts

--- src/App.js
+++ src/App.js
@@ -6,20 +6,11 @@ import Profile from "./components/Profile";
 import LogIn from "./components/LogIn";
 import SignUp from "./components/SignUp"
 import NavBar from "./components/NavBar";
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 import About from "./components/About";
-
-=======
-import Restaurant from "./components/Restaurant"
->>>>>>> b8e88fa483d35e10e0169eaa71d1c96d9eb77689
-=======
-
 import Food from "./components/Food"
 
 import Restaurant from "./components/Restaurant"
-
->>>>>>> 5fab2015ea8f0fc19cfd3e575a6c069a41412689
 import "./App.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -34,23 +25,12 @@ function App() {
         <Route path="/cart" element={<Cart />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/login" element={<LogIn />} />
-<<<<<<< HEAD
         <Route path="/signup" element={<SignUp />} />
         <Route path="/about" element={<About />} />
-<<<<<<< HEAD
-=======
-
         <Route path="/restaurant" element={<Restaurant />} />
->>>>>>> b8e88fa483d35e10e0169eaa71d1c96d9eb77689
-      </Routes>
-
-      
-=======
         <Route path="/food" element={<Food />} />
       </Routes>
-     
->>>>>>> 5fab2015ea8f0fc19cfd3e575a6c069a41412689
-    </>
+   </>
   );
 }
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,12 +11,8 @@ function Home() {
         <i className="fa fa-map-marker"></i>
           Enter Delivery Address
       </Link>
-<<<<<<< HEAD
-      <Link to="/restaurants" className="order-btn">
-=======
     
       <Link to="/restaurant" className="order-btn">
->>>>>>> 5fab2015ea8f0fc19cfd3e575a6c069a41412689
         <i className="fa fa-cutlery"></i>Order Now
       </Link>
 

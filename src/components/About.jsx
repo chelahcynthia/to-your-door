d7826c5f05a97e66379a67bafffa4971c36cf0ea
JeremyKungu <jeremikungu27@gmail.com>
2023-01-17 09:19:00 +0300

    added spans

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,7 +13,22 @@ function About() {
           <h3>TO YOUR DOOR</h3>
         </div>
         <div className='about-card3'>
-          <p>TO YOUR DOOR</p>
+          <p>We're a Kenyan tech company and the fastest-growing multicategory player in Africa. With technology at the core of our business, we create innovative solutions that connect customers, businesses and couriers.</p>
+        </div>
+        <div className='about-card4'>
+          <h3>40 COUNTIES</h3>
+        </div>
+        <div className='about-card5'>
+          <h4>10K COURIERS AND EMPLOYEES</h4>
+        </div>
+        <div className='about-card6'>
+          <p>We're a Kenyan tech company and the fastest-growing multicategory player in Africa. With technology at the core of our business, we create innovative solutions that connect customers, businesses and couriers.</p>
+        </div>
+        <div className='about-card7'>
+          <p>We're a Kenyan tech company and the fastest-growing multicategory player in Africa. With technology at the core of our business, we create innovative solutions that connect customers, businesses and couriers.</p>
+        </div>
+        <div className='about-card8'>
+          <h3>120 RESTAURANTS</h3>
         </div>
       </div>
     </>

5d97739869cf57c23c26faf716fd1b1d23f00710
JeremyKungu <jeremikungu27@gmail.com>
2023-01-17 09:00:58 +0300

    styles

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import "./About.css"
 
 function About() {
   return (
     <>
-      <h2 className='about-header'> About Us</h2>
-      
+      <h2 className='about-header'> ABOUT US</h2>
+      <div className='about-container'>
+        <div className='about-card1'>
+          <h3>FROM ANYWHERE...</h3>
+        </div>
+        <div className='about-card2'>
+          <h3>TO YOUR DOOR</h3>
+        </div>
+        <div className='about-card3'>
+          <p>TO YOUR DOOR</p>
+        </div>
+      </div>
     </>
   )
 }

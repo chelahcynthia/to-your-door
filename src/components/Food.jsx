dd30dfab5c9d35816265775b9b35ce47b82fe006
+ <+>
2023-01-17 11:28:01 +0300

    mapped the data from yhe backend to the frontend

--- src/components/Food.jsx
+++ src/components/Food.jsx
@@ -1,47 +1,27 @@
-import React, {useState, useEffect} from "react";
-import { Link} from "react-router-dom"
-import Button from "react-bootstrap/Button";
-import Card from "react-bootstrap/Card";
-import Form from "react-bootstrap/Form";
- import {products } from "../productsStore";
- import Cart from "./Cart";
+import React, { useState, useEffect } from "react";
 // import { getProductData } from "../productsStore";
 import "./Food.css";
-// import { CartContext } from "../CartContext"
-// import { useContext } from "react";
-import { CardGroup } from "react-bootstrap";
-// import Col from 'react-bootstrap/Col';
-// import Row from 'react-bootstrap/Row';
-import { CartState } from "../Context";
-
+// import { CartState } from "../Context";
 import SingleProduct from "./SingleProduct";
 
 function Food() {
-const {state: {products},} = CartState();
-// console.log(products);
+  // const {state: {products},} = CartState();
+  // console.log(products);
 
-  const [foodData, setFoodData] = useState([])
+  const [foodData, setFoodData] = useState([]);
 
- 
   useEffect(() => {
     fetch("http://localhost:9000/meals")
-    .then((res) => res.json())
-    .then((data) =>
-    setFoodData(data));
+      .then((res) => res.json())
+      .then((data) => setFoodData(data));
   }, []);
 
-  console.log(foodData)
-
-
+  // console.log(foodData)
   return (
-    <>
-              <div>{foodData.map((meal)=> {
-                return <SingleProduct meal = {meal} key = {meal.id} />
-              })}</div>
-           
-
-
-</>
+    <div className="food">
+        
+        <SingleProduct foodData={foodData} />
+      </div>
   );
 }
 export default Food;
--- src/components/SingleProduct.js
+++ src/components/SingleProduct.js
@@ -7,57 +7,72 @@ import Row from 'react-bootstrap/Row';
 import { CartState } from "../Context";
 
 
-const SingleProduct = ({meal}) => {
+const SingleProduct = ({foodData}) => {
     const {state: {cart}, dispatch, } = CartState();
-    console.log(cart)
+    // console.log(cart,)
+    console.log(foodData);
     return (
-        <Row xs={1} md={2} className="g-4">
+      <>
+     
+    
+    <div className="food">
+           
+       {foodData.map((food) => (
+        <>        
+        {/* <h1>{food.name}</h1>;     */}
+        <Row xs={5} md={2} className="g-4">
      
       
-        <Col key = {meal.id}>
-          <Card className="food-card">
-        <Card.Img src={meal.path} />
-        <Card.Body>
-        <Card.ImgOverlay>
-          <Card.Title className="title-color">{meal.mealName}</Card.Title>
-          <Card.Subtitle className="mb-2 text-muted subtitle-color">Price: {meal.price}</Card.Subtitle>
-          
-          <Card.Text className="text-color">
-           {meal.description}
-          </Card.Text>
-          <Card.Text className="food-cat">{meal.category}</Card.Text>
-              <Form>
-              <Form.Group className="quantity-box">
-                <div className="order-action">
-                <Form.Control type="number" placeholder="QTY"></Form.Control>
-                
-                <Button variant="secondary" >ADD TO CART</Button>
-             {cart.some((p) => p.id === meal.id) ? (
-                <Button onClick = {()=> {
-                    dispatch({
-                        type: "REMOVE_FROM_CART",
-                        payload: meal,
-                    })
-                 }} variant="danger">Remove from Cart</Button>
-                
-             ): (<Button disabled={!meal.inStock} onClick = {()=> {
-                dispatch({
-                    type: "ADD_TO_CART",
-                    payload: meal,
-                })
-             }}>{!meal.inStock ? "Out of Stock": "Add to Cart"}</Button>)}
-                </div>
-              </Form.Group>
-            </Form>
-          </Card.ImgOverlay>
-        </Card.Body>
-      </Card>
-     <Form>
+     <Col key = {food.id}>
+       <Card className="food-card">
+     <Card.Img src={food.img_url} />
+     <Card.Body>
+     <Card.ImgOverlay>
+       <Card.Title className="title-color">{food.foodName}</Card.Title>
+       <Card.Subtitle className="mb-2 text-muted subtitle-color">Price: {food.price}</Card.Subtitle>
+       
+       <Card.Text className="text-color">
+        {food.description}
+       </Card.Text>
+       <Card.Text className="food-cat">{food.category}</Card.Text>
+           <Form>
+           <Form.Group className="quantity-box">
+             <div className="order-action">
+             <Form.Control type="number" placeholder="QTY"></Form.Control>
+             
+             <Button variant="secondary" >ADD TO CART</Button>
+          {cart.some((p) => p.id === food.id) ? (
+             <Button onClick = {()=> {
+                 dispatch({
+                     type: "REMOVE_FROM_CART",
+                     payload: food,
+                 })
+              }} variant="danger">Remove from Cart</Button>
+             
+          ): (<Button disabled={!food.inStock} onClick = {()=> {
+             dispatch({
+                 type: "ADD_TO_CART",
+                 payload: food,
+             })
+          }}>Add to Cart</Button>)}
+             </div>
+           </Form.Group>
+         </Form>
+       </Card.ImgOverlay>
+     </Card.Body>
+   </Card>
+  <Form>
+   
+  </Form>
+ </Col>
+       </Row>
+       
+        </>
+      ))}
       
-     </Form>
-    </Col>
+    </div>
+</>
     
-          </Row>
     )
 }
 export default SingleProduct;

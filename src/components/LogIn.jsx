9cb584e8635bb5b846cfd9dbebe26ad5890b4508
cynthia <cchelangat9892gmail.com>
2023-01-18 20:08:33 +0300

    Fixed the home button color

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -1,16 +1,10 @@
-
-import React, {useState} from "react";
-
+import React, { useState } from "react";
 
 import "./LogIn.css";
 
-
 import { Link } from "react-router-dom";
 
-
-
-
-function LogIn({onLogIn, setUser}) {
+function LogIn({ onLogIn, setUser }) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [errors, setErrors] = useState();
@@ -20,34 +14,31 @@ function LogIn({onLogIn, setUser}) {
     console.log("i have been clicked");
 
     const formData = {
-      username:username,
-      password:password
-    }
+      username: username,
+      password: password,
+    };
 
     fetch("http://localhost:3000/login", {
-     method: "POST",
-     headers: {
-      "Content-Type": "application/json",
-     },
-     body: JSON.stringify(formData), 
-    })
-    .then((response) => {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    }).then((response) => {
       if (response.status > 199 && response.status < 300) {
-        response.json()
-        .then((data) => {
+        response.json().then((data) => {
           console.log(data);
-          localStorage.setItem("user", JSON.stringify(data.customer))
-          localStorage.setItem("token", data.jwt)
-        })
+          localStorage.setItem("user", JSON.stringify(data.customer));
+          localStorage.setItem("token", data.jwt);
+        });
       } else {
-        response.json()
-        .then((err) => {
+        response.json().then((err) => {
           console.log(err);
-          setErrors(err.error)
-        })
+          setErrors(err.error);
+        });
       }
-    })
-  }
+    });
+  };
 
   return (
     <div className="login-container">
@@ -55,29 +46,44 @@ function LogIn({onLogIn, setUser}) {
         <h6>Welcome to</h6>
         <h1>To Your Door App</h1>
       </div>
-      { errors ? (<div style={{color: "red", margin: "1rem"}}>{errors}</div>) : ""}
+      {errors ? (
+        <div style={{ color: "red", margin: "1rem" }}>{errors}</div>
+      ) : (
+        ""
+      )}
       <div className="main">
         <form>
           <span>
             <i className="fa fa-user icons"></i>
-            <input type="text" placeholder="Username" name="username" onChange={(e) => setUsername(e.target.value)}></input>
+            <input
+              type="text"
+              placeholder="Username"
+              name="username"
+              onChange={(e) => setUsername(e.target.value)}
+            ></input>
           </span>
           <br></br>
 
           <span>
             <i className="fa fa-lock"></i>
-            <input type="password" placeholder="password" name="password" onChange={(e) => setPassword(e.target.value)}></input>
+            <input
+              type="password"
+              placeholder="password"
+              name="password"
+              onChange={(e) => setPassword(e.target.value)}
+            ></input>
           </span>
           <br></br>
 
-          <button type="submit" onClick={(e) => handleSubmit(e)} id="login-btn">Login</button>
+          <button type="submit" onClick={(e) => handleSubmit(e)} id="login-btn">
+            Login
+          </button>
 
           <div className="links">
-                <p>Don't have an account?&nbsp;</p>
-                <Link to="/signup">Signup</Link>
-            </div>
+            <p>Don't have an account?&nbsp;</p>
+            <Link to="/signup">Signup</Link>
+          </div>
         </form>
-        
       </div>
     </div>
   );

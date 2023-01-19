d5c06c71e90cbfb360ee2ca71ce5e4cd00c2e0d3
cynthia <cchelangat9892gmail.com>
2023-01-19 09:00:52 +0300

    styled the checkout popup

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,76 +1,101 @@
-import React, {useState, useEffect} from 'react'
-import {CartState} from "../Context"
-import {ListGroup, Image} from "react-bootstrap"
+import React, { useState, useEffect } from "react";
+import "./Cart.css";
+import { CartState } from "../Context";
+import { ListGroup, Image } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 import Form from "react-bootstrap/Form";
 const Cart = () => {
+  const {
+    state: { cart },
+    dispatch,
+  } = CartState();
+  const [total, setTotal] = useState("");
+  const [num, setNum] = useState(0);
+  const [show, setShow] = useState(false);
+  // function for calculating random number
+  function randomNumberInRange(min, max) {
+    // 👇️ get number between min (inclusive) and max (inclusive)
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
 
-const {state: { cart}, dispatch,} = CartState();
-const [total, setTotal] = useState("");
-const [num, setNum] = useState(0);
-const [show, setShow] = useState(false)
-// function for calculating random number
-function randomNumberInRange(min, max) {
-  // 👇️ get number between min (inclusive) and max (inclusive)
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+  const handleClick = () => {
+    setNum(randomNumberInRange(1, 50));
+  };
 
-const handleClick = () => {
-  setNum(randomNumberInRange(1, 50));
-};
-
-
-useEffect(() => {
- setTotal(cart.reduce((acc, curr) => acc + Number(curr.price)*curr.qty, 0));
-},[cart])
+  useEffect(() => {
+    setTotal(
+      cart.reduce((acc, curr) => acc + Number(curr.price) * curr.qty, 0)
+    );
+  }, [cart]);
   return (
-    <div className="home">
-      {/* <div className="productContainer">
-      
+    <div className="checkout">
+      <div className="productContainer">
         {cart.map((food) => (
           <ListGroup.Item key={food.id}>
-         <Row>
-          <Col md={2}>
-            <Image src={food.path} alt={food.mealName} fluid rounded />
-          </Col>
-          <Col md={2} >
-            <span>{food.mealName}</span>
-          </Col>
-          <Col md={2} >
-            {food.price}
-          </Col>
-          <Col>
-         
-          <Form.Control type="number" placeholder="QTY" value={food.qty}
-          onChange={(e) => dispatch({type: "CHANGE_CART_QTY", payload:{
-            id:food.id,
-            qty: e.target.value,
-          }})}>
-         
-          </Form.Control>
-          </Col>
-          <Col>
-          <Button type="button" variant="light" onClick={() => dispatch({
-            type: "REMOVE_FROM_CART",
-            payload:food,
-          })}>Remove</Button>
-          </Col>
-         </Row>
+            <Row className="checkout_row">
+              <Col md={1}>
+                <Image src={food.img_url} alt={food.mealName} className="checkout_image" />
+              </Col>
+              <Col md={2}>
+                <span>{food.mealName}</span>
+              </Col>
+              <Col md={2}>{food.price}</Col>
+              <Col>
+                <Form.Control
+                  type="number"
+                  placeholder="QTY"
+                  value={food.qty}
+                  onChange={(e) =>
+                    dispatch({
+                      type: "CHANGE_CART_QTY",
+                      payload: {
+                        id: food.id,
+                        qty: e.target.value,
+                      },
+                    })
+                  }
+                ></Form.Control>
+              </Col>
+              <Col>
+                <Button
+                  type="button"
+                  variant="light"
+                  onClick={() =>
+                    dispatch({
+                      type: "REMOVE_FROM_CART",
+                      payload: food,
+                    })
+                  }
+                >
+                  Remove
+                </Button>
+              </Col>
+            </Row>
           </ListGroup.Item>
         ))}
-     
-      </div> */}
-      <div className='filters summary'>
+      </div>
+      <div className="filters-summary">
         <span className="title">Subtotal({cart.length}) items</span>
-        <span style={{fontWeight: 700, fontSize: 20}}>Total: Ksh {total}</span>
-     
-       {show && <h2>Your order will arrive in {num} minutes.</h2>}
-       <Button onClick={()=> setShow(!show)}>Add Delivery?</Button> 
-        <Button type="button" onClick={handleClick} disabled={cart.length === 0}>Proceed to Checkout</Button>
+        <span style={{ fontWeight: 700, fontSize: 20 }}>
+          Total: Ksh {total}
+        </span>
+
+        {show && <h2>Your order will arrive in {num} minutes.</h2>}
+        <Button className="Add-btn" onClick={() => setShow(!show)}>
+          Add Delivery?
+        </Button>
+        <Button
+          className="Checkout-btn"
+          type="button"
+          onClick={handleClick}
+          disabled={cart.length === 0}
+        >
+          Proceed to Checkout
+        </Button>
       </div>
     </div>
-  )
-}
+  );
+};
 export default Cart;
